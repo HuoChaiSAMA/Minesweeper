0dec51283f52e53ad3f5ad400f4c392cfbf2de20
HuoChaiSAMA <HuoChaiSAMA666@163.com>
2024-12-13 21:06:22 +0800

    On branch main Initial commit Changes to be committed: 	new file:   Minesweeper.cpp 	new file:   README.md

---
+++ Minesweeper.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    // freopen("Minesweeper.in", "r", stdin);
+    // freopen("Minesweeper.out", "w", stdout);
+
+    return 0;
+}
